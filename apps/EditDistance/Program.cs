2f14ec17021c5e03761c3ce5c8d45808e50aba5b
Martin Péchal <martin@pechal.net>
2014-03-31 22:31:44 +0200

    Testování generování edit distance

---
+++ apps/EditDistance/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditDistance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string word = "acress";
+
+            var result = DamerauLevensteinEditDistance.GenerateSubtitutions(word);
+
+        }
+    }
+}
