0f1fe7d2ba03929b3d1a102f4edb50a0b23f2a75
Martin Péchal <martin@pechal.net>
2014-03-31 22:31:23 +0200

    Testovací appka

---
+++ SpellCheckerConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using MPSpell;
+using MPSpell.Dictionary;
+using MPSpell.Dictionary.Parsers;
+using MPSpell.Check;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpellCheckerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DictionaryBuilder builder = new DictionaryBuilder(
+                new DefaultDictionaryFileParser(),
+                new DefaultAffixFileParser()
+            );
+
+            //var dictionary = builder.BuildDictionary("dictionaries/en_US/en_US.dic", "dictionaries/en_US/en_US.aff");
+            var dictionary = builder.BuildDictionary("dictionaries/cs_CZ/cs_CZ.dic", "dictionaries/cs_CZ/cs_CZ.aff");
+
+
+            FileChecker checker = new FileChecker(dictionary);
+            var output = checker.CheckFile("test.txt");
+
+
+        }
+
+
+
+    }
+}
